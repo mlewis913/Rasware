8406876f2982b29d8e3c48297c74b457510746fc
mlewis913 <lewismatthew913@gmail.com>
2018-11-14 20:51:35 -0600

    Update Main.c

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,166 +1,87 @@
 #include <RASLib/inc/common.h>
+#include <RASLib/inc/linesensor.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
 #include <RASLib/inc/motor.h>
 #include <RASLib/inc/adc.h>
 
-// Blink the LED to show we're on
-tBoolean blink_on = true;
-
-void blink(void) 
-{
-    SetPin(PIN_F3, blink_on);
-    blink_on = !blink_on;
-}
-
-//initializes the 6 ADC pins for the line sensor
-static tADC *adc[3];
-static tADC *adc2[3];
-static tBoolean initialized = false;
-void initGPIOLineSensor(void)
-{	
-    if(initialized)
-	{
-	    return;
-	}
-	
-    initialized = true;	
-
-    adc[0] = InitializeADC(PIN_D0);
-    adc[1] = InitializeADC(PIN_D1);
-    adc[2] = InitializeADC(PIN_D2);
-
-}
-
-void initGPIODistanceSensor(void)
-{
-	if(initialized)
-	{
-		return;
-	}
-	
-	initialized = true;
-
-	adc2[0] = InitializeADC(PIN_D3);
-	adc2[1] = InitializeADC(PIN_D4);
-	adc2[2] = InitializeADC(PIN_D5);
-
-}
-
-//displays values of the linesensor and distance sensor, use to find what colors
-//correspond to which values
-void gpioLineSensorDemo(void)
-{
-    while(!KeyWasPressed())
-	{
-	    Printf("Line Sensor values: %1.3f, %1.3f, %1.3f\r" + ADCRead(adc[0]) + ADCRead(adc[1]) + ADCRead(adc[2]));
-	}
-
-    Printf("\n");
-
-}
-
-void IRSensorDemo(void)
-{
-    while(!KeyWasPressed())
-	{
-	    Printf("Distance Sensor values: %1.3f, %1.3f, %1.3f\r" + ADCRead(adc2[0]) + ADCRead(adc2[1]) + ADCRead(adc2[2]));
-	}
-
-    Printf("\n");
-
-}
-
-//decleares motors
 static tMotor *left;
 static tMotor *right;
+static tADC *adc[1];
+static tADC *adc2[1];
+static tBoolean initialized = false;
 
+void initLineSensor(void){
+    if(initialized)
+        return;
+    initialized = true;
+}
 
+tBoolean led_on;
+void blink(void) {
+    SetPin(PIN_F1, led_on);
+    SetPin(PIN_F3, !led_on);
 
+    led_on = !led_on;
+}
 
-
-// The 'main' function is the entry point of the program
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) 
 {
-	// Initialization code can go here
-    
-	CallEvery(blink, 0, 0.5);
-    
-	left = InitializeServoMotor(PIN_B6, false);
-	right = InitializeServoMotor(PIN_B7, true);
+    CallEvery(blink, 0, 0.5f);
+    left = InitializeServoMotor(PIN_E0, false);
+    right = InitializeServoMotor(PIN_D3, true);
+    adc[0] = InitializeADC(PIN_E2);
+    int b1 = ADCRead(adc[0]) * 4096;
+	//line sensor intialization
+    adc2[0] = InitializeADC(PIN_E1);
+    int b2 = ADCRead(adc2[0]) * 4096;
+	//distance sensor intialization
 
-    //infinite loops are bad, but for the purpose
-    //of a never ending sumo battle that is started and stopped 
-    //by a drop it will suffice
-	/*while(1 > 0)
-	{
-		//SenorRead();
-		//DistanceRead();
-	}*/
-   
-}
+    while(1)
+    {
+		if(ADCRead(adc2[0]) * 4096 < 1000) 
+		{
+		SetMotor(left, .75);
+		SetMotor(right, -.75);
+		Wait(.2);
+		}
+		if(ADCRead(adc2[0]) * 4096 < 1000) 
+		{
+		SetMotor(left, -.75);
+		SetMotor(right, .75);
+		Wait(.2);
+		}
+		if(ADCRead(adc2[0]) * 4096 < 1000) 
+		{
+		SetMotor(left, -.75);
+		SetMotor(right, .75);
+		Wait(.2);
+		}
+		if(ADCRead(adc2[0]) * 4096 < 1000) 
+		{
+		SetMotor(left, .75);
+		SetMotor(right, -.75);
+		Wait(.2);
+		}
+	//sweeping detection motion
+		else if(ADCRead(adc2[0]) * 4096 >= 1000) 
+		{
+		SetMotor(left, 1);
+		SetMotor(right, 1);
+		}
+	//charge motion if within about 3 feet
+		if (ADCRead(adc[0]) * 4096 <= 2000)
+		{
+		SetMotor(left, -1);
+		SetMotor(right, -1);
+                Wait(.35);
+		SetMotor(left, -1);
+		SetMotor(right, 1);
+		Wait(.85);
+		} 
+	//motion if reached edge
+    }
 
-
-void reverse()
-{
-	SetMotor(left,-1.0);
-	SetMotor(right, -1.0);
-}
-
-void forward()
-{
-	SetMotor(left, 1.0);
-	SetMotor(right, 1.0);
-}
-
-void left()
-{
-	SetMotor(left, -1.0);
-	SetMotor(right, 1.0);
-}
-
-void right()
-{
-	SetMotor(left, 1.0);
-	SetMotor(right, -1.0);
-}
-
-//need to know values before proceeding
-/*
-void SensorRead()
-{
-	int x = 0;
-	int y = 1;
-	int z = 2;
-	//qualifiers needed for middle values like .5 between .75 and .2.5
-	if(ADCRead(adc[x]) > && ADCRead(adc[y]) >  && ADCRead(adc[z]) > )
-	{
-		reverse();
-	}
-	
-	if(ADCRead(adc[x]) >  && ADCRead(adc[y]) >  && ADCRead(adc[z]) > )
-	{
-		reverse();
-	}
-}
-*/
-
-/*
-//not sure how many values or how the values
-//are returned for the distance sensor yet
-void DistanceRead()
-{
-	int x = 0;
-	int y = 1;
-	int z = 2;
-	//qualifiers needed for middle values like .5 between .75 and .2.5
-	if(ADCRead(adc2[x]) > && ADCRead(adc2[y]) >  && ADCRead(adc2[z]) > )
-	{
-		reverse();
-	}
-	
-	if(ADCRead(adc2[x]) >  && ADCRead(adc2[y]) >  && ADCRead(adc2[z]) > )
-	{
-		reverse();
-	}
 }
